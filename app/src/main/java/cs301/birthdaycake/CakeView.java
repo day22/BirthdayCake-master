e70432c625c90f85562c601223bf835311cecc0f
Aidan Day <day22@up.edu>
2020-09-17 16:29:36 -0700

    shotened candles

--- app/src/main/java/cs301/birthdaycake/CakeView.java
+++ app/src/main/java/cs301/birthdaycake/CakeView.java
@@ -31,7 +31,7 @@ public class CakeView extends SurfaceView {
     public static final float frostHeight = 50.0f;
     public static final float candleHeight = 300.0f;
     public static final float candleWidth = 40.0f;
-    public static final float wickHeight = 30.0f;
+    public static final float wickHeight = 20.0f;
     public static final float wickWidth = 6.0f;
     public static final float outerFlameRadius = 30.0f;
     public static final float innerFlameRadius = 15.0f;
